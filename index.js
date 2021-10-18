29b51f0dd290e29149323dd07251e63e5a7d0e07
Pasha <rubber883@gmail.com>
2021-10-18 23:26:16 +0300

    first commit

--- index.js
+++ index.js
@@ -4,17 +4,9 @@ const zlib = require('zlib');
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
-
-
 const server = http.createServer((req, res) =>{
-    // let downloadUrl = req.url;
     let downloadUrl = req.url.replace('/api/downloads/?', '');
     console.log(downloadUrl);
-    // const downloadPath = "C:/Users/L-64_E5570/Downloads/downloaded-image.png.gz";
-    // const write = fs.createWriteStream(downloadPath);
-    // const zip = zlib.createGzip();
-    // res.pipe(zip).pipe(write);
-    // res.end()
     http.get(downloadUrl, (res)=>{
         const path = "C:/Users/L-64 E5570/Downloads/downloaded-image.png.gz";
         const write = fs.createWriteStream(path);
@@ -24,26 +16,10 @@ const server = http.createServer((req, res) =>{
     res.end()
 })
 
-
-    //     }
-    // }
-
-
 server.listen(PORT, ()=>{
     console.log(`Server started on : ${PORT}`)
 })
 
-// const url = "https://sebhastian.com/img/default.png";
-
-    // https.get(url, (res)=>{
-    //         const path = "downloaded-image.png";
-    //         const filePath = fs.createWriteStream(path);
-    //         res.pipe(filePath);
-    //         filePath.on('finish',() => {
-    //             filePath.close();
-    //             console.log('Download Completed');
-    //         })
-    // })
 
 
 
